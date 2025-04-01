84e766aaf18942dee3e45b0147f427aec80c33b7
Márton Gergő0 <martongergo.dev@gmail.com>
2025-04-01 15:12:16 +0200

    Factory class is added

---
+++ src/Provider/CipherServiceProvider.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Provider;
+
+use App\LookupTable\LookupTableDTO;
+use App\Crypto\CryptoInterface;
+
+class CipherServiceProvider
+{
+    private LookupTableDTO $lookupTable;
+    private CryptoInterface $crypto;
+    private string $secretKey;
+
+    public function __construct(LookupTableDTO $lookupTable, CryptoInterface $crypto, string $secretKey) {
+        $this->lookupTable = $lookupTable;
+        $this->crypto = $crypto;
+        $this->secretKey = $secretKey;
+    }
+
+    public function getLookupTable(): LookupTableDTO
+    {
+        return $this->lookupTable;
+    }
+
+    public function getCrypto(): CryptoInterface
+    {
+        return $this->crypto;
+    }
+
+    public function encode(string $message): string
+    {
+        $table = $this->lookupTable;
+        $key = $this->secretKey;
+        return $this->crypto->encrypt($message, $table, $key);
+    }
+
+    public function decode(string $secret): string
+    {
+        $table = $this->lookupTable;
+        $key = $this->secretKey;
+        return $this->crypto->decrypt($secret, $table, $key);
+    }
+
+    public function setSecretKey(string $secter): void
+    {
+        $this->secretKey = $secter;
+    }
+    
+}
