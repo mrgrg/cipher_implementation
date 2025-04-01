5c4f0f59090f3361aa077e2101a831de25b7a03d
Márton Gergő0 <martongergo.dev@gmail.com>
2025-04-01 15:13:16 +0200

    Test initial implementation

--- index.php
+++ index.php
@@ -1,7 +1,18 @@
 <?php
 
+use App\Crypto\BasicCrypto;
+use App\LookupTable\BasicLookupTable;
+use App\Provider\CipherServiceProvider;
+
 require_once 'vendor/autoload.php';
 
-use App\Test;
+$secretKey = 'xy';
 
-Test::run();
+$chars = "abcdefghijklmnopqrstuvwxyz ";
+$lookupTable = BasicLookupTable::generateFromString($chars);
+$crypto = new BasicCrypto("testKey");
+
+$chiper = new CipherServiceProvider($lookupTable, $crypto, $secretKey);
+
+var_dump($chiper->decode("qwertz"));
+var_dump($chiper->encode("qwertz"));
